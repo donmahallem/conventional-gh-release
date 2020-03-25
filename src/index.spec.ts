a7e43c9589e52128e7602c387f274ea2c18d8e80
donmahallem <donmahallem@users.noreply.github.com>
2020-03-25 14:54:32 +0100

    chore: init

---
+++ src/index.spec.ts
@@ -0,0 +1,12 @@
+/*!
+ * Source https://github.com/donmahallem/conventional-gh-release
+ */
+
+import { expect } from 'chai';
+import 'mocha';
+
+describe('index', () => {
+    it('should contain FlowApiValidator', () => {
+        expect(true).to.not.be.eq(false);
+    });
+});
